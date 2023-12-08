eb76c5703071a920320a4c32972ec33e7ef91cfc
Dani <danial@Danis-MacBook-Pro.local>
2023-12-08 03:31:49 +0330

    update sidebar and add appbar styles

--- src/app/dashboard/_ui/navbar/navbar.tsx
+++ src/app/dashboard/_ui/navbar/navbar.tsx
@@ -1,7 +1,33 @@
+"use client";
 import React from "react";
+import styles from "./navbar.module.css";
+import { usePathname } from "next/navigation";
+
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch,
+} from "react-icons/md";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const pathName = usePathname();
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>{pathName.split("/").pop()}</div>
+      <div className={styles.menu}>
+        <div className={styles.search}>
+          <MdSearch />
+          <input type="text" placeholder="Search..." className={styles.input} />
+        </div>
+        <div className={styles.icons}>
+          <MdPublic size={20} />
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/app/dashboard/_ui/sidebar/sidebar.tsx
+++ src/app/dashboard/_ui/sidebar/sidebar.tsx
@@ -105,6 +105,10 @@ function Sidebar() {
           </li>
         ))}
       </ul>
+      <button className={styles.logout}>
+        <MdLogout />
+        Logout
+      </button>
     </div>
   );
 }
