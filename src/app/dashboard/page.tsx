50e36241a6e4d890351da4b4434b3151c40f9c6f
Dani <danial@Danis-MacBook-Pro.local>
2023-12-10 23:56:06 +0330

    update dashboard and add base layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import styles from "./dashboard.module.css";
+import Card from "./_ui/card/card";
+import RightBar from "./_ui/rightbar/rightbar";
+import TransActions from "./_ui/transActions/transActions";
+import Chart from "./_ui/chart/chart";
 
 function DashboardPage() {
-  return <div>DashboardPage</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <TransActions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <RightBar />
+      </div>
+    </div>
+  );
 }
 
 export default DashboardPage;
